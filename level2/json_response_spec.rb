6a082bb02c51d836f6d4f5fa54548c1c5ab47689
Valentin Birault <biraultvalentin@gmail.com>
2020-07-04 16:17:38 +0200

    level2 tests added

---
+++ level2/json_response_spec.rb
@@ -0,0 +1,11 @@
+require 'json'
+require_relative '../lib/json_response'
+require '../spec/spec_helper'
+
+RSpec.describe 'JsonResponse' do 
+  it 'returns expected json output' do
+    json_response = JsonResponse.new(json_input: '../level2/data/input.json')
+    json_expected = JSON.parse(open('../level2/data/expected_output.json').read)
+    expect(json_response.prices_generator.to_json).to eq(json_expected.to_json)
+  end
+end 
