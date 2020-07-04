26347fb0b84404db68223eb95056adad4dffc91c
Valentin Birault <biraultvalentin@gmail.com>
2020-07-04 16:08:06 +0200

    level1 folders reorganisation

--- level1/main.rb
+++ level1/main.rb
@@ -1,6 +1,6 @@
 require 'json'
-require_relative 'json_response'
+require_relative '../lib/json_response'
 
-response = JsonResponse.new(json_input: "level1/data/input.json")
+response = JsonResponse.new(json_input: "../level1/data/input.json")
 @hash_prices = response.prices_generator.to_json
 puts @hash_prices
