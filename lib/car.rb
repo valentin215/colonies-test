25538a00f3630dbbc3dbfb7a267fdb341a756c34
Valentin Birault <biraultvalentin@gmail.com>
2020-07-04 16:11:03 +0200

    added new folder lib with main classes

---
+++ lib/car.rb
@@ -0,0 +1,10 @@
+class Car
+  attr_accessor :informations, :id, :price_per_day, :price_per_km
+
+  def initialize(informations:)
+    @informations = informations
+    @id = informations['id']
+    @price_per_day = informations['price_per_day']
+    @price_per_km = informations['price_per_km']
+  end 
+end 
