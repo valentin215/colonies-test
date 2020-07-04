a39d550701daf00ab1460de5e89df5966fbc6515
Valentin Birault <biraultvalentin@gmail.com>
2020-07-04 17:56:29 +0200

    level2 done / tests passed

--- level2/main.rb
+++ level2/main.rb
@@ -1,6 +1,6 @@
 require 'json'
-require_relative '../level1/json_response.rb'
+require_relative '../lib/json_response.rb'
 
-response = JsonResponse.new(json_input: "level2/data/input.json")
+response = JsonResponse.new(json_input: "../level2/data/input.json")
 @hash_prices = response.prices_generator.to_json
 puts @hash_prices
--- lib/price.rb
+++ lib/price.rb
@@ -15,6 +15,32 @@ class Price
   end 
 
   def rental_price
-    (car.price_per_day * rental.rental_days) + (car.price_per_km * rental.distance)
+    (total_price_per_day) + (car.price_per_km * rental.distance)
+  end 
+
+  private 
+
+  def total_price_per_day
+    if rental.rental_days > 1 && rental.rental_days < 5
+      price_after_one_day
+    elsif rental.rental_days >= 5 && rental.rental_days < 10
+      price_after_four_days
+    elsif rental.rental_days > 10
+      price_after_ten_days
+    else 
+      car.price_per_day
+    end 
+  end
+  
+  def price_after_one_day
+    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * (rental.rental_days - 1)
+  end 
+
+  def price_after_four_days
+    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * 3 + (car.price_per_day - (car.price_per_day * 30/100)) * (rental.rental_days - 4)
+  end 
+
+  def price_after_ten_days
+    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * 3 + (car.price_per_day - (car.price_per_day * 30/100)) * 6 + (car.price_per_day - (car.price_per_day * 50/100)) * (rental.rental_days - 10)
   end 
 end 
