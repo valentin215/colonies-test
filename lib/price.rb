f3b3c04a6b20138826a771bbdc5142a616b2d33e
Valentin Birault <biraultvalentin@gmail.com>
2020-07-05 15:37:12 +0200

    level5 second refactoring

--- lib/price.rb
+++ lib/price.rb
@@ -34,17 +34,17 @@ class Price
   end 
 
   def drivy_fee
-    (drivy_fee_without_options) + rental.drivy_options_credit
+    (drivy_fee_without_options) + drivy_options_credit
   end 
 
   def owner_credit
-    (rental_price_without_options) - (insurance_fee + assistance_fee + (drivy_fee_without_options)) + rental.owner_options_credit
+    (rental_price_without_options) - (insurance_fee + assistance_fee + (drivy_fee_without_options)) + owner_options_credit
   end 
 
   private
   
   def rental_price_without_options
-    rental_price - rental.options_price
+    rental_price - options_price
   end 
 
   def drivy_fee_without_options
@@ -59,19 +59,53 @@ class Price
     elsif rental.rental_days > 10
       price_after_ten_days
     else 
-      car.price_per_day + rental.options_price
+      car.price_per_day + options_price
     end 
   end
   
   def price_after_one_day
-    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * (rental.rental_days - 1) + rental.options_price
+    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * (rental.rental_days - 1) + options_price
   end 
 
   def price_after_four_days
-    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * 3 + (car.price_per_day - (car.price_per_day * 30/100)) * (rental.rental_days - 4) + rental.options_price
+    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * 3 + (car.price_per_day - (car.price_per_day * 30/100)) * (rental.rental_days - 4) + options_price
   end 
 
   def price_after_ten_days
-    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * 3 + (car.price_per_day - (car.price_per_day * 30/100)) * 6 + (car.price_per_day - (car.price_per_day * 50/100)) * (rental.rental_days - 10) + rental.options_price
+    car.price_per_day + (car.price_per_day - (car.price_per_day * 10/100)) * 3 + (car.price_per_day - (car.price_per_day * 30/100)) * 6 + (car.price_per_day - (car.price_per_day * 50/100)) * (rental.rental_days - 10) + options_price
+  end 
+
+  def options_price
+    @options_price ||= begin
+      if rental.has_options
+        rental.options.map do |option|
+          (option.price_per_day * rental.rental_days)
+        end.sum 
+      else
+        0
+      end
+    end 
+  end
+  
+  def owner_options_credit
+    if rental.has_owner_credit_options
+      rental.options.map do |option|
+        next if option.type == 'additional_insurance'
+        (option.price_per_day * rental.rental_days)
+      end.sum 
+    else
+      0
+    end
+  end
+  
+  def drivy_options_credit
+    if rental.has_drivy_credit_options
+      rental.options.map do |option|
+        next if option.type != 'additional_insurance'
+        (option.price_per_day * rental.rental_days)
+      end.sum
+     else 
+      0
+    end 
   end 
 end 
--- lib/rental.rb
+++ lib/rental.rb
@@ -11,14 +11,14 @@ class Rental
     @start_date = Date.parse(informations['start_date'])
     @end_date = Date.parse(informations['end_date'])
     @distance = informations['distance']
-    @options = find_options
+    @options = linked_options
   end 
 
   def rental_days
     (@end_date - @start_date).to_i + 1
   end
   
-  def find_options
+  def linked_options
     @find_options ||= begin
       arr = []
       Option.all.each do |option|
@@ -39,52 +39,17 @@ class Rental
     options.any?
   end 
 
-  def has_owner_fee_options
+  def has_owner_credit_options
     options.each do |option|
       return true if option.type == 'gps' || option.type == 'baby_seat' 
     end
     false 
   end
 
-  def has_drivy_fee_options
+  def has_drivy_credit_options
     options.each do |option|
       return true if option.type == 'additional_insurance'
     end
     false 
-  end 
-
-  def options_price
-    @options_price ||= begin
-      if self.has_options
-        self.options.map do |option|
-          (option.price_per_day * self.rental_days)
-        end.sum 
-      else
-        0
-      end
-    end 
-  end  
-
-  
-  def owner_options_credit
-    if self.has_owner_fee_options
-      self.options.map do |option|
-        next if option.type == 'additional_insurance'
-        (option.price_per_day * self.rental_days)
-      end.sum 
-    else
-      0
-    end
-  end
-
-  def drivy_options_credit
-    if self.has_drivy_fee_options
-      self.options.map do |option|
-        next if option.type != 'additional_insurance'
-        (option.price_per_day * self.rental_days)
-      end.sum
-     else 
-      0
-    end 
   end 
 end 
